374cb3a0a80504b8ce733dcfcc2a10ff2c630982
rafly <raflyadityaray3@gmail.com>
2024-09-03 15:56:45 +0700

    katalog-project

---
+++ database/migrations/2024_09_03_002308_create_produks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produks', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_produk')->unique();
+            $table->text('deskripsi');
+            $table->integer('harga');
+            $table->unsignedBigInteger('id_kategori');
+            $table->string('cover');
+            $table->timestamps();
+
+            $table->foreign('id_kategori')->references('id')->on('kategoris')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produks');
+    }
+};
